e1de16ed47e61aeee63f076e83b8aed6e7692fde
Sebastian Germesin <neogermi@googlemail.com>
2012-06-28 15:08:05 +0200

    DBPediaService.load() returns a Collection of entities, than an array, addressing issue #115

--- test/service/dbpedia.js
+++ test/service/dbpedia.js
@@ -1,6 +1,6 @@
 module("vie.js - DBPedia Service");
 
-test("Test DBPedia connection", function() {
+test("VIE.js DBPedia connection", function() {
     var z = new VIE();
     ok(z.DBPediaService, "Checking if the DBPedia Service exists.'");
     z.use(new z.DBPediaService);
@@ -23,7 +23,40 @@ test("VIE.js DBPediaService - Load", function () {
     .execute()
     .done(function(x) {
         ok(x, "Something returned");
-        ok(x.isEntity);
+        ok(x.isCollection);
+        ok(x.at(0).isEntity);
+        start();
+    })
+    .fail(function(f){
+        ok(false, f.statusText);
+        start();
+    });
+});
+
+
+
+test("VIE.js DBPediaService - Load multiple entities", function () {
+    if (navigator.userAgent === 'Zombie') {
+       return;
+    } 
+    var entity1 = "<http://dbpedia.org/resource/Barack_Obama>";
+    var entity2 = "<http://dbpedia.org/resource/London>";
+    var z = new VIE();
+    ok (z.DBPediaService);
+    equal(typeof z.DBPediaService, "function");
+    z.use(new z.DBPediaService());
+    stop();
+    z
+    .load({entity: [entity1, entity2]})
+    .using('dbpedia')
+    .execute()
+    .done(function(x) {
+        ok(x, "Something returned");
+        ok(x.isCollection);
+        ok(x.at(0).isEntity);
+        ok(x.at(1).isEntity);
+        equal(x.at(0).id, entity1);
+        equal(x.at(1).id, entity2);
         start();
     })
     .fail(function(f){
